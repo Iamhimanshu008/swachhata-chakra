e8ff5949ec1d7a887d1055af72b8822cdc3c67d4
Himanshu Shekhar <himanshu2992004@gmail.com>
2026-04-03 18:40:46 +0530

    fix: add missing Platform import in RecyclersScreen

--- mobile/src/screens/public/RecyclersScreen.js
+++ mobile/src/screens/public/RecyclersScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Linking, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Linking, ActivityIndicator, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import api from '../../api/client';
 import Constants from 'expo-constants';
